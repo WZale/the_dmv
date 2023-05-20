065cde028255224ccb0c5ccb28e7c2b8e6b67876
William Zale <williamzale@gmail.com>
2023-05-20 17:16:58 -0400

    test: Add test for administer_road_test

--- spec/facility_spec.rb
+++ spec/facility_spec.rb
@@ -108,5 +108,32 @@ RSpec.describe Facility do
       expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
     end
   end
-
+  
+  describe "#administer_road_test" do
+  it "can administer a road test" do
+    # registrant 3
+    expect(@facility_1.administer_road_test(@registrant_3)).to eq(false)
+    
+    @registrant_3.earn_permit
+    
+    expect(@facility_1.administer_road_test(@registrant_3)).to eq(false)
+    expect(@registrant_3.license_data).to eq({:written=>false, :license=>false, :renewed=>false})
+    
+    # registrant 1
+    expect(@facility_1.administer_road_test(@registrant_1)).to eq(false)
+    
+    @facility_1.add_service("Written Test")
+    @facility_1.add_service("Road Test")
+    
+    expect(@facility_1.administer_written_test(@registrant_1)).to eq(true)
+    expect(@facility_1.administer_road_test(@registrant_1)).to eq(true)
+    expect(@registrant_1.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
+    
+    # registrant 2
+    @registrant_2.earn_permit
+    expect(@facility_1.administer_written_test(@registrant_2)).to eq(true)
+    expect(@facility_1.administer_road_test(@registrant_2)).to eq(true)
+    expect(@registrant_2.license_data).to eq({:written=>true, :license=>true, :renewed=>false})
+    end
+  end
 end
