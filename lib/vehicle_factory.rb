2728e71953ad53be3c21f431ea8f7d588d141640
William Zale <williamzale@gmail.com>
2023-05-21 17:02:04 -0400

    feat: Add initial class structure and create_vehicles method

---
+++ lib/vehicle_factory.rb
@@ -0,0 +1,14 @@
+require "vehicle"
+
+class VehicleFactory
+  def initialize
+
+  end
+
+  def create_vehicles(registrations)
+    registrations.map do |registration| 
+      Vehicle.new( {vin: registration[:vin_1_10], year: registration[:model_year], make: registration[:make], model: registration[:model], engine: :ev} )
+    end
+  end
+
+end
